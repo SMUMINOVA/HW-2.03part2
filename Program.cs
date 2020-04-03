248ca11ce14eef4bc1f4d17d436042d4c1396d43
Sobira <sobira204@gmail.com>
2020-04-03 19:34:10 +0500

    create class Book

--- Program.cs
+++ Program.cs
@@ -27,6 +27,22 @@ namespace hw_1._03p2
                 System.Console.WriteLine($"Content of book is {content}");
             }
         }
-
+        class Book{
+            public string authorOfBook {get; set;}
+            public string titleOfBook{get; set;}
+            public string contentOfBook{get; set;}
+            public void ShowAuthor(){
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                System.Console.WriteLine($"Author of book is {authorOfBook}");
+            }
+            public void ShowTitle(){
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                System.Console.WriteLine($"Title of book is {titleOfBook}");
+            }
+            public void ShowContent(){
+                Console.ForegroundColor = ConsoleColor.Green;
+                System.Console.WriteLine($"Content of book is {contentOfBook}");
+            }
+        }
     }
 }
